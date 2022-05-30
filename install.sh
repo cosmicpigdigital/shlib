9201f2b52a1e59c54297b57ee05966f601e417d2
mateus-earth <matt@cosmicpig.digital>
2022-05-30 10:51:30 -0300

    [bugfix] Copy the contents of source dir, not the directory itself.

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@
 ##
 ##
 INSTALL_DIR="$HOME/.cosmic/libs/shlib";
-rm    -rfv             "$INSTALL_DIR";
-mkdir -pv              "$INSTALL_DIR";
-cp    -Rv  "./source/" "$INSTALL_DIR";
+rm    -rfv              "$INSTALL_DIR";
+mkdir -pv               "$INSTALL_DIR";
+cp    -Rv  ./source/*   "$INSTALL_DIR";
